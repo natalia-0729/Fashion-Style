b35f675dad13598f80f78f086f5ce11b44a25eeb
natalia-0729 <you@example.com>
2022-10-20 19:33:44 +0300

    added slider sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,15 @@
 $(function(){
-  $('.services__inner').slick();
-
-
-
+  $('.services__inner').slick({
+    prevArrow: false,
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="Стрелка вправо"></button>',
+  });
+  $('.comment__inner').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="Стрелка влево"></button>',
+    nextArrow: '<button type="button" class="slick-next slick-next__comment"><img src="images/arrow-right.svg" alt="Стрелка вправо"></button>',
+  });
+  $('.team__inner').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="Стрелка влево"></button>',
+    nextArrow: '<button type="button" class="slick-next slick-next__comment"><img src="images/arrow-right.svg" alt="Стрелка вправо"></button>',
+  });
 
 });
